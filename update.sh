5bb05db18ba77790336a411c0e76b53b813331fd
Rodrigo Kassick <kassick@gmail.com>
2016-07-22 12:32:13 -0300

    fix update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,4 @@ echo "Pulling changes"
 git pull origin master
 
 echo "Updating submodules"
-
-git submodule foreach 'git checkout master'
-git submodule foreach 'git pull origin'
+git submodule foreach "(git checkout master; git pull; cd ..; git add '$OLDPWD'; git commit -m 'Submodule Sync')"
