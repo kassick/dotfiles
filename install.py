78b5e5ba0d6723d9688f8084538c59e4a6c1bfe4
Rodrigo Kassick <kassick@gmail.com>
2016-07-22 00:20:18 -0300

    Install script (untested)

---
+++ install.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# File: "/home/kassick/Sources/dotfiles/install.py"
+# Created: "Thu Jul 21 21:46:45 2016"
+# Updated: "2016-07-22 00:17:36 kassick"
+# $Id$
+# Copyright (C) 2016, Rodrigo Kassick
+
+import os
+import os.path as path
+import sys
+import re
+import subprocess
+
+SELECTION_FILE="dot_select.txt"
+
+def add_dot(dots, name, path, install, bundle):
+    if name in dots:
+        dots[name] = tuple(apply(lambda i: i[0] if i[1] is None else i[1],
+                           zip([path, install, bundle], dots[name])))
+    else:
+        dots[name] = (path, install, bundle)
+
+def rec_file_split(p):
+    if len(p) == 0:
+        return []
+    elif p == '/':
+        return ['/']
+    else:
+        (h, t) = path.split(p)
+        return rec_file_split(h) + [t]
+
+
+def files_conflict_resolve(src, dst, renames, removes, installs):
+    if os.path.exists(dst):
+        if os.path.islink(dst):
+            removes.append(dst)
+            installs.append(( src, dst) )
+        else:
+            print "\tFile %s already exists" % dst
+            print "\tAction:"
+            print "\t\t (o)verride"
+            print "\t\t (b)ackup"
+            print "\t\t (s)kip"
+
+            action = None
+            while not(action in ['o', 'b', 's']):
+                action = sys.stdin.readline().strip()
+
+            if action == 'o':
+                removes.append(dst)
+                installs.append( (src, dst) )
+            elif action == 'b':
+                renames.append( (dst, dst + '.orig') )
+                installs.append( (src, dst) )
+            pass
+        pass
+    else:
+        installs.append( (src, dst) )
+
+def dot_install(dot, dot_path, dot_install, dot_bundle):
+    removes = []
+    renames = []
+    dirs = []
+    installs = []
+
+    if dot_path:
+        print "dot/%s --> ~/.%s" % (dot_path, dot_path)
+
+        dest = path.expanduser(path.join('~', '.' + dot_path ))
+        src = path.join('dot', dot_path)
+
+        files_conflict_resolve(src, dest, renames, removes, installs )
+
+    if dot_bundle:
+        print "Install bundle %s " % dot
+        for root, dirnames, filenames in os.walk(path.join('bundle', dot)):
+            destbase = os.path.sep.join(rec_file_split(root)[2:]) # ditch bundle/dotname
+
+            for d in dirnames:
+                do_mkdir = True
+                if len(destbase) == 0:
+                    dest_dir = os.path.expanduser(os.path.join("~", "." + d))
+                else:
+                    dest_dir = os.path.expanduser(os.path.join('~', '.' + destbase, d))
+                if os.path.exists(dest_dir):
+                    if os.path.isdir(dest_dir):
+                        do_mkdir = False
+                    else:
+                        print "\tTarget %s is not a directory" % destk_dir
+                        print "\t\t (b)ackup"
+                        print "\t\t (i)gnore"
+                        action = None
+                        while not(action in ['b', 'i']):
+                            action = sys.stdin.readline().strip()
+
+                        if action == 'b':
+                            os.rename(dest_dir, dest_dir + ".orig")
+                        else:
+                            do_mkdir = False
+                        pass
+                    pass
+
+                if do_mkdir:
+                    dirs.append(dest_dir)
+
+            for f in filenames:
+                if len(destbase) == 0:
+                    dest_path = os.path.expanduser(os.path.join("~", '.' + f))
+                else:
+                    dest_path = os.path.expanduser(os.path.join("~", "." + destbase, f))
+
+                src_file = path.join(root, f)
+
+                files_conflict_resolve(src_file, dest_path, renames, removes, installs )
+            pass
+        pass
+
+    print "Verification:"
+    print "Will remove:"
+    for f in removes:
+        print "\t", f
+    print ""
+    print "Will Rename:"
+    for s,d in renames:
+        print "\t", s, "->", d
+    print ""
+    print "Will Create:"
+    for d in dirs:
+        print "\t", d
+    print ""
+    print "Will Install:"
+    for s, d in installs:
+        print "\t", s, "->", d
+
+    print ""
+
+    print "(C)onfirm, (Abort) ?"
+    action = None
+    while not(action in ['A', 'C']):
+        action = sys.stdin.readline().strip()
+
+    if action == 'A':
+        return
+
+    print "Removing ..."
+    for r in removes:
+        if path.isfile(r):
+            os.path.remove(r)
+        else:
+            os.removedirs(r)
+
+    print "Renaming ..."
+    for s, d in renames:
+        os.rename(s, d)
+
+    print "Installing ..."
+    for s, d in installs:
+        os.symlink(s, d)
+
+    if dot_install:
+        print "Executing ", dot_install
+        subprocess.call([dot_install], cwd=os.path.expanduser("~"))
+
+def find_all_dots():
+    dots = {}
+    # dots maps name => (path, install, bundle)
+    #     if path is set, a link is created on home
+    #     if bundle is set, then every file inside bundle/name is linked to home and following paths
+    #     if install is set, the program will be executed afterwards
+
+    install_re = re.compile(r'^INSTALL_(?P<dot>[a-z0-9][a-z0-9\.]+)\.[a-z0-9]+$')
+
+    for dot in os.listdir('dot'):
+        if not(install_re.match(dot)):
+            add_dot(dots, dot, dot, None, None)
+
+    for dot in os.listdir('install'):
+        script = os.path.join('install', dot)
+        add_dot(dots, dot, None, script, None)
+
+    for dot in os.listdir('bundle'):
+        add_dot(dots, dot, None, None, dot )
+
+    for cur in ['dot', 'bundle', 'install']:
+        l = os.listdir(cur)
+        for dot in l:
+            match = install_re.match(dot)
+            if match:
+                script = os.path.join(cur, do)
+                add_dot(dots, match.groupdict()['dot'], None, script, None)
+    return dots
+
+def main():
+    dots = find_all_dots()
+    if path.exists(SELECTION_FILE):
+        selection = open(SELECTION_FILE).readlines()
+    else:
+        selection = dots.keys()
+
+    print "Selected dots:", "\n".join(apply(lambda i: " - " + i, selection))
+
+    print ""
+
+    for dot in selection:
+        print "Installing dot '%s'" % dot
+        dot_install(dot, *dots[dot])
+
+        print "---------------------------------"
+
+if __name__ == "__main__": main()
