bc376e9cf7f61808aec60c96222d36a93a1a92c8
Rodrigo Kassick <kassick@gmail.com>
2016-07-27 00:36:47 -0300

    Update update.sh script

--- update.sh
+++ update.sh
@@ -6,3 +6,13 @@ git pull origin master
 
 echo "Updating submodules"
 git submodule foreach "(git checkout master; git pull; cd ..; git add '$OLDPWD'; git commit -m 'Submodule Sync')"
+
+for sub in dot/*/.git bundle/*/.git install/*/.git; do
+    sub=`dirname "$sub"`
+    if [ -d "$sub" ]; then
+        pushd "$sub"
+        git pull
+        popd
+    fi
+
+done
