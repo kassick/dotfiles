78ad946bd30bc84d3a56c5fa0285c683791cae4d
Rodrigo Kassick <kassick@gmail.com>
2017-01-24 01:48:20 -0200

    Fix update

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ git pull origin master
 echo "Updating submodules"
 git submodule foreach "(git checkout master; git pull; cd ..; git add '$OLDPWD'; git commit -m 'Submodule Sync')"
 
-for sub in dot/*/.git bundle/*/.git install/*/.git; do
+for sub in dots/*/.git bundle/*/.git install/*/.git; do
     sub=`dirname "$sub"`
     if [ -d "$sub" ]; then
         pushd "$sub"
