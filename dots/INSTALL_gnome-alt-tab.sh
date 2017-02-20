98e64b7ba5af88a36a048c0fffd1bbbcc2529856
Rodrigo Kassick <kassick@gmail.com>
2017-02-20 14:06:26 -0300

    Script to adjust gnome alt-tab automatically on a new session

---
+++ dots/INSTALL_gnome-alt-tab.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Alt><Shift>Tab']"
