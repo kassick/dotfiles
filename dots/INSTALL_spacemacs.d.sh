234777c56892a1b403a20c021917acc76be98430
Rodrigo Kassick <kassick@gmail.com>
2017-06-23 17:51:25 -0300

    Spacemacs instead of custom emacs.d

---
+++ dots/INSTALL_spacemacs.d.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Install spacemacs in .emacs.d
+
+REPO="https://github.com/syl20bnr/spacemacs"
+DEST=~/.emacs.d
+
+if [ -d $DEST ]; then
+    cd $DEST
+    if [ -d .git ]; then
+        if git remote show origin -n | grep -q $REPO; then
+            git pull origin master
+            exit 0
+        fi
+    fi
+
+    echo "Old install, moving out of the way"
+    cd ~
+    mv $DEST ~/__emacs.d.bk
+fi
+
+mkdir $DEST && \\
+git init && \\
+git remote add origin $REPO && \\
+git pull origin master || \\
+exit 1
+
+# Needed by pdftools
+pkcon install poppler-devel
+pkcon install poppler-glib-devel
