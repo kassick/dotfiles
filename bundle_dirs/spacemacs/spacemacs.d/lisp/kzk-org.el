a2388b34116594f5263d9d2c2a6945311d1b1f2b
Rodrigo Kassick <kassick@gmail.com>
2017-06-24 11:56:06 -0300

    Fixes to have ditaa working

--- bundle_dirs/spacemacs/spacemacs.d/lisp/kzk-org.el
+++ bundle_dirs/spacemacs/spacemacs.d/lisp/kzk-org.el
@@ -65,47 +65,6 @@
 
 
 (with-eval-after-load 'org
-;;; Ditaa does not exechute as a jar nowadays, geesh
-;;; Have to redefine the function 'cause babel wants to pass
-;;; a hell lot of inexistent parameters and then the command
-;;; line has a lot of stray arguments that ditaa has no idea
-;;; how to handle ...
-  (defun org-babel-execute:ditaa (body params)
-    "Execute a block of Ditaa code with org-babel.
-                        This function is called by `org-babel-execute-src-block'."
-    (let* ((result-params (split-string (or (cdr (assoc :results params)) "")))
-           (out-file (let ((el (cdr (assoc :file params))))
-                       (or el
-                           (error
-                            "ditaa code block requires :file header argument"))))
-           (cmdline (cdr (assoc :cmdline params)))
-           (java (cdr (assoc :java params)))
-           (in-file (org-babel-temp-file "ditaa-"))
-           (eps (cdr (assoc :eps params)))
-           (eps-file (when eps
-                       (org-babel-process-file-name (concat in-file ".eps"))))
-           (pdf-cmd (when (and (or (string= (file-name-extension out-file) "pdf")
-                                   (cdr (assoc :pdf params))))
-                      (concat
-                       "epstopdf"
-                       " " eps-file
-                       " -o=" (org-babel-process-file-name out-file))))
-           (cmd (concat org-babel-ditaa-java-cmd
-                        " " java
-                        " " cmdline
-                        " " (org-babel-process-file-name in-file)
-                        " " (if pdf-cmd
-                                eps-file
-                              (org-babel-process-file-name out-file)))))
-      (unless (file-exists-p org-ditaa-jar-path)
-        (error "Could not find ditaa.jar at %s" org-ditaa-jar-path))
-      (with-temp-file in-file (insert body))
-      (message cmd) (shell-command cmd)
-      (when pdf-cmd (message pdf-cmd) (shell-command pdf-cmd))
-      nil))
-
-
-
   (general-define-key :keymaps 'org-mode-map
                       "C-c d" 'insert-date
                       "C-c t" 'insert-time-date
@@ -167,27 +126,6 @@
   (add-hook 'org-mode-hook 'org-display-inline-images)
   (add-hook 'org-mode-hook 'org-babel-result-hide-all)
 
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '(
-     (latex . t)
-     (shell . t)
-     (python . t)
-     (ditaa . t)
-     (dot . t)
-     (sqlite . t)
-     (perl . t)
-     (screen . t)
-     (org . t)
-     (makefile . t)
-     ))
-
-  ;; Dita settings
-  (setq org-babel-ditaa-java-cmd "/usr/bin/ditaa"
-        org-ditaa-eps-jar-path "/"
-        org-ditaa-jar-option "-jar"
-        org-ditaa-jar-path "/")
-
   (add-to-list 'org-structure-template-alist
                '("r" "#+BEGIN_SRC R :results output :session *R* :exports both\n\n#+END_SRC" "<src lang=\"R\">\n\n</src>"))
 
@@ -233,6 +171,73 @@
     (add-to-list 'org-latex-default-packages-alist '("" "ifxetex" t))
     (add-to-list 'org-latex-default-packages-alist '("" "minted" t))
     )
+
+;;; Ditaa does not exechute as a jar nowadays, geesh
+;;; Have to redefine the function 'cause babel wants to pass
+;;; a hell lot of inexistent parameters and then the command
+;;; line has a lot of stray arguments that ditaa has no idea
+;;; how to handle ...
+  (with-eval-after-load 'ob-ditaa
+    ;; Dita settings
+    (setq org-babel-ditaa-java-cmd "/usr/bin/ditaa"
+          org-ditaa-eps-jar-path "/"
+          org-ditaa-jar-option "-jar"
+          org-ditaa-jar-path "/"
+          org-babel-default-header-args:ditaa '((:results . "file")
+                                                (:exports . "results")
+                                                (:java . "-e UTF-8"))
+          )
+    (defun org-babel-execute:ditaa (body params)
+      "Execute a block of Ditaa code with org-babel.
+                        This function is called by `org-babel-execute-src-block'."
+      (let* ((result-params (split-string (or (cdr (assoc :results params)) "")))
+             (out-file (let ((el (cdr (assoc :file params))))
+                         (or el
+                             (error
+                              "ditaa code block requires :file header argument"))))
+             (cmdline (cdr (assoc :cmdline params)))
+             (java (cdr (assoc :java params)))
+             (in-file (org-babel-temp-file "ditaa-"))
+             (eps (cdr (assoc :eps params)))
+             (eps-file (when eps
+                         (org-babel-process-file-name (concat in-file ".eps"))))
+             (pdf-cmd (when (and (or (string= (file-name-extension out-file) "pdf")
+                                     (cdr (assoc :pdf params))))
+                        (concat
+                         "epstopdf"
+                         " " eps-file
+                         " -o=" (org-babel-process-file-name out-file))))
+             (cmd (concat org-babel-ditaa-java-cmd
+                          " " java
+                          " " cmdline
+                          " " (org-babel-process-file-name in-file)
+                          " " (if pdf-cmd
+                                  eps-file
+                                (org-babel-process-file-name out-file)))))
+        (unless (file-exists-p org-ditaa-jar-path)
+          (error "Could not find ditaa.jar at %s" org-ditaa-jar-path))
+        (with-temp-file in-file (insert body))
+        (message "Executando comando %S" cmd)
+        (shell-command cmd)
+        (when pdf-cmd (message pdf-cmd) (shell-command pdf-cmd))
+        nil))
+  )
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '(
+     (latex . t)
+     (shell . t)
+     (python . t)
+     (ditaa . t)
+     (dot . t)
+     (sqlite . t)
+     (perl . t)
+     (screen . t)
+     (org . t)
+     (makefile . t)
+     ))
+
   ;;
 
 
