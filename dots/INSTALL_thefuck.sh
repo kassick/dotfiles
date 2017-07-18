ec6f8ceff50cee1d02c933e604d32e8ac7b4f47c
Rodrigo Kassick <kassick@gmail.com>
2017-07-18 15:05:25 -0300

    Missing package in fedora wtf

--- dots/INSTALL_thefuck.sh
+++ dots/INSTALL_thefuck.sh
@@ -3,6 +3,7 @@
 echo "Installing python3-dev and python3-pip"
 pkcon install -y python3-devel python3-dev
 pkcon install -y python3-pip
+pkcon install -y redhat-rpm-config
 
 echo "Installing thefuck"
 if [ -f $HOME/.local/bin/thefuck ]; then
