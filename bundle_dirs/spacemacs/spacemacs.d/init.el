96d10ccf3fd294747c84ea56c7e5c5c28aa07ad2
Rodrigo Kassick <rodrigo.kassick@corp.globo.com>
2019-07-01 12:00:23 -0300

    Spacemacs: default font set to Meslo

--- bundle_dirs/spacemacs/spacemacs.d/init.el
+++ bundle_dirs/spacemacs/spacemacs.d/init.el
@@ -213,8 +213,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro" ; "Meslo LG S for Powerline"
-                               :height 130
+   dotspacemacs-default-font '("Meslo LG S for Powerline"
+                               :height 110
                                :weight normal
                                :width normal
                                :powerline-scale 1.0)
