3f558f6f0d502c2425c5b40afb1f36d96a79b918
Rodrigo Kassick <kassick@gmail.com>
2020-06-01 23:59:36 -0300

    fix[emacs]: use correct fira font :/

--- bundle_dirs/spacemacs/spacemacs.d/init.el
+++ bundle_dirs/spacemacs/spacemacs.d/init.el
@@ -279,7 +279,7 @@ values."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '(("Fira Code"
+   dotspacemacs-default-font '(("Fira Code Retina"
                                 :height 120
                                 :weight normal
                                 :width normal
