1492e7895452c516586b02bd894095fc89e3ef98
Rodrigo Kassick <rodrigo.kassick@corp.globo.com>
2020-08-21 00:54:49 -0300

    chore[emacs]: elisp style

--- bundle_dirs/spacemacs/spacemacs.d/lisp/kzk-org.el
+++ bundle_dirs/spacemacs/spacemacs.d/lisp/kzk-org.el
@@ -61,11 +61,11 @@
 
   (require 'kzk-org-babel)
 
+  ;; Extra packages for org-mode
   (require 'org-tempo)
   (require 'ox-extra)
   (ox-extras-activate '(ignore-headlines))
-  (require 'org-protocol)
-)
+  (require 'org-protocol))
 
 ;; LaTeX settings
 (with-eval-after-load 'ox-latex
