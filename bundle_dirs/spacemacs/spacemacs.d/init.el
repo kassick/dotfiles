79c18c21d6228afca47be3f4643b8d771ea425d4
Rodrigo Virote Kassick <rodrigo.kassick@ifood.com.br>
2021-09-07 18:19:50 -0300

    fix[emacs]: enable version-control layer
    Without the layer, emacs may load incomplete magit features, which I kind of
    expect to be always available

--- bundle_dirs/spacemacs/spacemacs.d/init.el
+++ bundle_dirs/spacemacs/spacemacs.d/init.el
@@ -59,7 +59,7 @@ This function should only modify configuration layer settings."
                       auto-completion-enable-help-tooltip 'manual
                       )
      git
-     ;; version-control
+     version-control
      ;; (treemacs :variables treemacs-use-git-mode 'deferred)
      (spell-checking :variables
                      spell-checking-enable-by-default nil
