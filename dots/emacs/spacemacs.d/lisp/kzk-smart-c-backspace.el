eb03e574b4987919d8abe710bfb1d353dcc2e956
Rodrigo Kassick <kassick@gmail.com>
2022-02-13 16:00:10 -0300

    refactor[emacs] use flat cond instead of nested ifs

--- dots/emacs/spacemacs.d/lisp/kzk-smart-c-backspace.el
+++ dots/emacs/spacemacs.d/lisp/kzk-smart-c-backspace.el
@@ -60,73 +60,70 @@
  def |
  |
 "
+  (let* ((opening-regex (sp--get-opening-regexp))
+         (closing-regex (sp--get-closing-regexp))
+         (closing-pair-and-spaces-regex (concat closing-regex "[[:space:]]+"))
+         (opening-pair-and-spaces-regex (concat opening-regex "[[:space:]]+")))
 
-  (if (and (not (clean-aindent--inside-indentp))
-           (bound-and-true-p smartparens-mode))
+    (cond
 
-      ;; def some(other) ->|    or
-      ;; def some(other) -> |   or
-      (if (kzk/smart-kill-looking-at-stopper-backwards t)
-            (progn
-              (delete-region (match-beginning 0) (match-end 0)) ; def some(other) |
-              (delete-horizontal-space t))                      ; def some(other)|
+     ;; in indent expressoin, fallback
+     ( (clean-aindent--inside-indentp) (apply fn args))
 
-        ;; def some(other) -> thing|
-        (-if-let (last-stopper (kzk/smart-kill-backwards-stopper-before-word))
-          (delete-region last-stopper (point)) ; def some(other) ->|
+     ;; def some(other) ->|    or
+     ;; def some(other) -> |   or
+     ( (kzk/smart-kill-looking-at-stopper-backwards t)
+       (progn
+         (delete-region (match-beginning 0) (match-end 0)) ; def some(other) |
+         (delete-horizontal-space t)) )                    ; def some(other)|
 
-          ;; now handle parens
-          (let* ((opening-regex (sp--get-opening-regexp))
-                 (closing-regex (sp--get-closing-regexp))
-                 (closing-pair-and-spaces-regex (concat closing-regex "[[:space:]]+"))
-                 (opening-pair-and-spaces-regex (concat opening-regex "[[:space:]]+")))
+     ;; def some( other: *|thing )
+     ( (kzk/smart-kill-backwards-stopper-before-word)
+       (delete-region (match-end 0) (point)) ) ; def some( other:thing )
 
-            (cond
+     ;; if not parens, fallback now
+     ( (not (bound-and-true-p smartparens-mode)) (apply fn args) )
 
-             ;; some ( thing )     |
-             ( (looking-back closing-pair-and-spaces-regex)
-               (delete-horizontal-space t))  ; some ( thing )|
+     ;; some ( thing )     |
+     ( (looking-back closing-pair-and-spaces-regex)
+       (delete-horizontal-space t))     ; some ( thing )|
 
-             ;; some( thing )|
-             ( (looking-back closing-regex)
-               (sp-backward-delete-char)     ; some( thing |)
-               (delete-horizontal-space t))  ; some( thing|)
+     ;; some( thing )|
+     ( (looking-back closing-regex)
+       ;; sp-backward-delete-char handles multi-char pairs and unbalanced ones too
+       (sp-backward-delete-char)        ; some( thing |)
+       (delete-horizontal-space t) )    ; some( thing|)
 
-             ;; some ( |thing )
-             ( (looking-back opening-pair-and-spaces-regex)
-               (delete-horizontal-space t))  ; some(|thing)
+     ;; some ( |thing )
+     ( (looking-back opening-pair-and-spaces-regex)
+       (delete-horizontal-space t))  ; some(|thing)
 
-             ;; some (|  ) thing      or
-             ;; some (|) thing        or
-             ;; some (|other) thing   or
-             ;; some (| } thing       or  (not a pair)
-             ;; some (|other } thing      (not a pair)
-             ( (looking-back opening-regex)
+     ;; some (|  ) thing      or
+     ;; some (|) thing        or
+     ;; some (|other) thing   or
+     ;; some (| } thing       or  (not a pair)
+     ;; some (|other } thing      (not a pair)
+     ;; thing will be some plist if we are inside a valid pair
+     ( (and (sp-get-thing t) (looking-back opening-regex))
+       (let* ((thing (sp-get-thing t))
+              (thing-beg (plist-get thing :beg))
+              (thing-end (plist-get thing :end))
+              (thing-inside-beg (+ thing-beg (length (plist-get thing :op))))
+              (thing-inside-end (- thing-end (length (plist-get thing :cl))))
+              (thing-contents (buffer-substring thing-inside-beg thing-inside-end)))
 
-               (-if-let (thing (sp-get-thing t))
-                   ;; thing will be some plist if we are inside a valid pair
-                   (let* ((thing-beg (plist-get thing :beg))
-                          (thing-end (plist-get thing :end))
-                          (thing-inside-beg (+ thing-beg (length (plist-get thing :op))))
-                          (thing-inside-end (- thing-end (length (plist-get thing :cl))))
-                          (thing-contents (buffer-substring thing-inside-beg thing-inside-end)))
+         (if (length= (string-trim thing-contents) 0)
+             ;; empty pair
+             (delete-region thing-beg thing-end) ; some | thing
 
-                   (if (length= (string-trim thing-contents) 0)
-                       ;; empty pair
-                       (delete-region thing-beg thing-end) ; some | thing
+           ;; else, non-empty pair
+           (goto-char thing-beg))                ; some |(other) thing
 
-                     ;; else, non-empty pair
-                     (goto-char thing-beg)                 ; some |(other) thing
-                     ;; either way, clean horizontal space to the left
-                     (delete-horizontal-space t))) ; some| thing    or   some|other thing
+         ;; either way, clean horizontal space to the left
+         (delete-horizontal-space t)) )          ; some| thing    or   some|other thing
 
-                 ;; if not thing, it's an unbalanced pair fallback to clean-aindent
-                 (apply fn args)))
-
-             ( t (apply fn args)) )) ))
-
-    ;; either at indent or no smartparens
-    (apply fn args)))
+     ;; fallback
+     ( t (apply fn args) ) )))
 
 (advice-add 'clean-aindent--bsunindent :around #'kzk/around-clean-aindent--bsunindent)
 (with-eval-after-load 'clean-aindent-mode
