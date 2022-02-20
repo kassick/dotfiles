e35dfdb35322143402f470059077e42388f52df5
Rodrigo Kassick <kassick@gmail.com>
2022-02-20 12:27:29 -0300

    fix: post-init company-posframe

--- dots/emacs/spacemacs.d/layers/kzk-company/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-company/packages.el
@@ -45,7 +45,7 @@
         )
 )
 
-(defun kzk-company/init-company-posframe ()
+(defun kzk-company/post-init-company-posframe ()
     ;;; this fixes an issue with a very slow posframe -- something in GNOME interferes with emacs frames causing delays
     ;;; https://github.com/tumashu/company-posframe/issues/2#issuecomment-609945180
     ;;; This is a workaround to have posframe working without 2sec lag under gnome
