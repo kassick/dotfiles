14e4046b9f4bb6e7e647be22c9f4d2d6c7927acd
Rodrigo Kassick <kassick@gmail.com>
2022-04-12 08:53:50 -0300

    chore[emacs]: evil-surround is now disabled in spacemacs
    Commit 9dd28c031688eb74c8d4a3233239f29250c5f7fb adds code to ensure surround is
    not enabled in a magit buffer

--- dots/emacs/spacemacs.d/layers/kzk-embrace/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-embrace/packages.el
@@ -1,4 +1,4 @@
-(defconst kzk-embrace-packages '(evil-embrace evil-surround magit))
+(defconst kzk-embrace-packages '(evil-embrace evil-surround))
 
 (defun kzk-embrace/init-evil-embrace ()
   (advice-add 'evil-embrace-enable-evil-surround-integration
@@ -11,7 +11,3 @@
 (defun kzk-embrace/post-init-evil-surround ()
   ;; pass
   )
-
-(defun kzk-embrace/post-init-magit ()
-  ;; force disable evil-surround in magit as it shadows s for staging
-  (add-hook 'magit-mode-hook (lambda () (evil-surround-mode -1))))
