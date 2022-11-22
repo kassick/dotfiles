3701bc20a0bdbdcbbac71c30d487128de1c3ced9
Rodrigo Kassick <kassick@gmail.com>
2022-11-22 13:05:08 -0300

    fix[emacs]: attempt to avoid lsp-signature replacing the contents of the buffer
    A bug (https://github.com/emacs-lsp/lsp-mode/issues/2767) causes the eldoc
    string to replace the whole buffer contents in some situations (right after
    saving a file with some syntax error). This change tries to avoid the bug
    trigger, as per the docummentation

--- dots/emacs/spacemacs.d/layers/kzk-lsp/config.el
+++ dots/emacs/spacemacs.d/layers/kzk-lsp/config.el
@@ -6,4 +6,9 @@
  ;; show less documentation lines in minibuffer. the default uses too much
  ;; vertical space
  '(lsp-signature-doc-lines 5)
+
+ ;; trying to avoid a bug where lsp signature inserts the contents of the
+ ;; docstring in the current buffer instead of the minibuffer
+ '(lsp-signature-function 'lsp-signature-posframe)
+ '(lsp-signature-auto-activate '(:on-server-request))
  )
