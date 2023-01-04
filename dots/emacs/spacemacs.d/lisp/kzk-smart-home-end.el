2eec38d2de0d4b2e5a8fc819fe9764692f720ae6
Rodrigo Kassick <kassick@gmail.com>
2023-01-04 12:18:31 -0300

    fix[emacs]: update ignored modes for smart home and end

--- dots/emacs/spacemacs.d/lisp/kzk-smart-home-end.el
+++ dots/emacs/spacemacs.d/lisp/kzk-smart-home-end.el
@@ -117,7 +117,7 @@
   :prefix "kzk-smart-home-end-mode")
 
 (defcustom kzk-smart-home-end-mode-ignored-major-modes
-  '(vterm-mode term-mode eshelm-mode helm-mode minibuffer-mode)
+  '(vterm-mode term-mode eshelm-mode helm-mode minibuffer-mode minibuffer-inactive-mode)
   "List of modes where the global mode won't be automatically initialized."
   :group 'kzk-smart-home-end
   :type '(repeat symbol))
