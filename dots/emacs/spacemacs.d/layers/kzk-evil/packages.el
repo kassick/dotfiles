543d0d79c26f56d4edee9a67725d04d407951e78
Rodrigo Kassick <kassick@gmail.com>
2023-03-20 09:47:57 -0300

    feat[emacs]: C-v is evil-paste

--- dots/emacs/spacemacs.d/layers/kzk-evil/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-evil/packages.el
@@ -4,13 +4,22 @@
 
 (defun kzk-evil/post-init-general ())
 
+(defun kzk-evil/paste (&optional COUNT REGISTER)
+  (interactive "*P")
+  (spacemacs/evil-mc-paste-before COUNT REGISTER)
+  (right-char))
+
 (defun kzk-evil/post-init-evil ()
 
   (kzk/after-init
    (message "Unleashing universal evil")
    (general-define-key :keymaps '(evil-evilified-state-map-original evil-evilified-state-map)
                        "C-u" 'universal-argument)
-   (define-key evil-insert-state-map (kbd "C-v") 'yank)
+   (define-key evil-insert-state-map (kbd "C-v") 'kzk-evil/paste)
+   (evil-define-key 'insert vterm-mode-map
+     (kbd "C-S-v") 'evil-collection-vterm-paste-after)
+
+
    (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
    (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
    (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
