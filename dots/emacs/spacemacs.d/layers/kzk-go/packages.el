4a736ca5a5ccf96e9ab0485216b2889b8943e72a
Rodrigo Kassick <kassick@gmail.com>
2023-08-17 10:58:42 -0300

    chore[emacs]: WIP regarding formatting key for Python and Go

--- dots/emacs/spacemacs.d/layers/kzk-go/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-go/packages.el
@@ -1,13 +1,9 @@
 (defconst kzk-go-packages '(go-mode))
 
 (defun kzk-go/post-init-go-mode ()
-  (message "not setting gofmt key")
-  (with-eval-after-load 'general
-    (general-define-key :keymaps 'go-mode-map
-                        :states '(normal motion visual)
-                        :prefix dotspacemacs-major-mode-leader-key
-                        "ZF" '(gofmt :which-key "Go FMT")))
-  ;; (spacemacs/set-leader-keys-for-major-mode 'go-mode
-  ;;   "=G" 'gofmt)
-  ;; (describe-map 'spacemacs-go-mode-map-prefix)
-  )
+  ;;(spacemacs/set-leader-keys-for-major-mode 'go-mode "==" 'lsp-format-buffer)
+  nil)
+
+  ;; (message "not setting gofmt key")
+  ;; (with-eval-after-load 'lsp-mode
+  ;;   (define-key spacemacs-lsp-mode-map (kbd "==") 'lsp-format-buffer)))
--- dots/emacs/spacemacs.d/layers/kzk-python/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-python/packages.el
@@ -5,6 +5,15 @@
 (custom-set-variables
  '(importmagic-style-configuration-alist '((multiline . backslash) (max_columns . 1200))))
 
+;; (defun kzk-python/post-init-lsp-mode ()
+;;   "Spacemacs python layer defines = as a prefix key in spacemacs-python-mode-map, which masks
+;;    spacemacs's lsp layer definition of =
+;;   "
+;;   ;;; DOES NOT WORK -- order of evaluation -- kzk-lsp will define == as format,
+;;   ;;; overriding this definition
+;;   (message "setting lsp-mode minor mode == from kzk-python")
+;;   (spacemacs/set-leader-keys-for-minor-mode 'lsp-mode "==" #'spacemacs/python-format-buffer))
+
 (defun kzk-python/post-init-smartparens ()
   (spacemacs/set-leader-keys-for-major-mode 'python-mode
     "re" 'kzk/py-refactor-extract-arg
