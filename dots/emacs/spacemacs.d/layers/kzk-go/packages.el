877b53dd304cf04fa867c77bea8c7b17010c3670
Rodrigo Kassick <kassick@gmail.com>
2023-09-12 18:55:32 -0300

    chore[emacs]: make lsp-go provide full documentation

--- dots/emacs/spacemacs.d/layers/kzk-go/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-go/packages.el
@@ -1,4 +1,4 @@
-(defconst kzk-go-packages '(go-mode))
+(defconst kzk-go-packages '(go-mode lsp-mode))
 
 (defun kzk-go/post-init-go-mode ()
   ;;(spacemacs/set-leader-keys-for-major-mode 'go-mode "==" 'lsp-format-buffer)
@@ -7,3 +7,7 @@
   ;; (message "not setting gofmt key")
   ;; (with-eval-after-load 'lsp-mode
   ;;   (define-key spacemacs-lsp-mode-map (kbd "==") 'lsp-format-buffer)))
+
+(defun kzk-go/post-init-lsp-mode ()
+  (custom-set-variables
+   '(lsp-go-hover-kind "FullDocumentation")))
