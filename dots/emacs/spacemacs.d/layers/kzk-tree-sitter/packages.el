427c989f248ad3be1302a7fc7baf89efc3fc90f0
Rodrigo Kassick <kassick@gmail.com>
2023-12-27 16:22:33 -0300

    fix[emacs]: force clean up of verboten tree sitter modes early
    If left to when the custom variable is set, the function ends up being called
    too late -- after a possibly problematic file is opened and emacs crashes

--- dots/emacs/spacemacs.d/layers/kzk-tree-sitter/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-tree-sitter/packages.el
@@ -2,5 +2,9 @@
   '(tree-sitter-langs))
 
 (defun kzk-tree-sitter/post-init-tree-sitter-langs ()
-  ;; Nothing to do here -- the defcustom already handles calling kzk/tree-sitter-remove-verboten-languages on update
+  ;; Force call here to make sure this happens in the startup process. If left
+  ;; only to be set by custom, it may not be correctly set up if a markdown
+  ;; file is the first file opened -- the hook happens after the tree sitter
+  ;; has been set up and loaded for the buffer... i.e. too late :/
+  (kzk/tree-sitter-remove-verboten-languages)
   )
