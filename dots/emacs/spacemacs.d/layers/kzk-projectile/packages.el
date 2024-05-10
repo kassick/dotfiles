88f6222b867b153b60550f5f378159e90024e1e4
Rodrigo Kassick <kassick@gmail.com>
2024-05-10 11:32:34 -0300

    fix[emacs]: marginalia/embark not guessing correct completion type in projectile

--- dots/emacs/spacemacs.d/layers/kzk-projectile/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-projectile/packages.el
@@ -1,4 +1,22 @@
 (defconst kzk-projectile-packages '(projectile))
 
 (defun kzk-projectile/post-init-projectile ()
-  (spacemacs/set-leader-keys "ps" 'projectile-save-project-buffers))
+  (spacemacs/set-leader-keys "ps" 'projectile-save-project-buffers)
+
+  (with-eval-after-load 'projectile
+    (advice-add 'projectile-find-file
+                :around (lambda (fn &rest args)
+                          ;; When projectile-find-file is called from the
+                          ;; projectile-completing-read :action parameter,
+                          ;; this-command is 'vertico-exit.
+
+                          ;; Marginalia uses this-command to check which kind
+                          ;; of item is present in the completion by looking
+                          ;; up against marginalia-command-categories.
+
+                          ;; Calling projectile-find-file from lisp does not
+                          ;; update this-command, so we need to take a hint
+                          ;; from embark-become and update it somewhere ...
+
+                          (let ((this-command 'projectile-find-file))
+                            (apply fn args))))))
