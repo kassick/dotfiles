169b124dbf672f2f109789da8ec025e79689609a
Rodrigo Kassick <kassick@gmail.com>
2024-05-15 19:05:21 -0300

    chore[emacs]: customize completions-annotations instead of marginalia-documentation
    Consult uses completion-annotations for explicit annotations during
    completion. Marginalia inherits from that face, so it is easier to update once
    and fix it everywhere ...

--- dots/emacs/spacemacs.d/init.el
+++ dots/emacs/spacemacs.d/init.el
@@ -904,7 +904,7 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
            (vterm-color-cyan  :foreground "#00ACC1" :background "#00ACC1")
            (vterm-color-green :foreground "#C3D82C" :background "#C3D82C")
            (vterm-color-white :foreground "#DEDDDA" :background "#DEDDDA")
-           (marginalia-documentation :foreground "#a3a3a3" :inherit (completions-annotations))
+           (completions-annotations :foreground "#a3a3a3" :inherit (italic shadow))
            )
 
           ;; --- END theming-modifications ---
