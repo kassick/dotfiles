05ec71b1f1bd79359cfce1b6ac24c285e8772209
Rodrigo Kassick <kassick@gmail.com>
2024-06-11 09:39:01 -0300

    fix[emacs]: bind flyspell wrapper independent of mode
    Spacemacs does not care about mode for spellcheck, so let's make it consistent

--- dots/emacs/spacemacs.d/layers/kzk-spell/packages.el
+++ dots/emacs/spacemacs.d/layers/kzk-spell/packages.el
@@ -3,11 +3,11 @@
 (defun kzk-spell/post-init-flyspell-correct ()
   ;; Helm flyspell
 
-  (kzk/after-init
-    (general-define-key :keymaps 'flyspell-mode-map
-                        "C-M-;" '("Correct Word" . flyspell-correct-wrapper))
 
-    (general-define-key :keymaps 'flyspell-mode-map
-                        :prefix dotspacemacs-leader-key
-                        :states '(normal motion visual)
-                        "S ;" '("Correct Word" . flyspell-correct-wrapper))))
+  (kzk/after-init
+   (message "Defining keys for flyspell-mode")
+   (define-key global-map
+               (kbd "C-M-;") '("Correct Word" . flyspell-correct-wrapper))
+   (spacemacs/set-leader-keys
+     "S ;" '("Correct Word" . flyspell-correct-wrapper)))
+  )
