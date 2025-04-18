963616f2cca3496499531916bdd078b2a0b459ea
Rodrigo Kassick <kassick@gmail.com>
2025-04-17 21:38:17 -0300

    chore[emacs]: font adjustments

--- dots/emacs/spacemacs.d/init.el
+++ dots/emacs/spacemacs.d/init.el
@@ -515,7 +515,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '(("Iosevka" :weight light :width expanded :size 9.8)
+   dotspacemacs-default-font '(("Iosevka" :weight light :width normal :size 13)
                                ;; ("Fira Code" :height 120)
                                ;; ("Meslo LG S for Powerline"
                                ;;  :height 113
