0fbe01effef470ca2f84170777a3e2a2b29532cc
Rodrigo Kassick <kassick@gmail.com>
2025-06-11 17:14:04 -0300

    chore[emacs]: commit missing file

---
+++ dots/emacs/spacemacs.d/layers/kzk-python/config.el
@@ -0,0 +1,5 @@
+(custom-set-variables
+ ;; disable this annoying behaviour, since pressing colon does not jump over
+ ;; the included colon
+ '(sp-python-insert-colon-in-function-definitions nil)
+ '(pytest-cmd-flags "-x"))
